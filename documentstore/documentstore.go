8f91c502ede0d4666b066c5020e856af91ebc5fc
Denys.Konovalov <denis.konov93@gmail.com>
2025-04-04 16:47:11 +0300

    Add document field type validation

--- documentstore/documentstore.go
+++ documentstore/documentstore.go
@@ -1,6 +1,8 @@
 package documentstore
 
-import "errors"
+import (
+	"errors"
+)
 
 var documentStore = map[string]Document{}
 
@@ -23,18 +25,49 @@ type Document struct {
 	Fields map[string]DocumentField
 }
 
+func (d Document) validate() error {
+	for key, field := range d.Fields {
+		switch field.Type {
+		case DocumentFieldTypeString:
+			if _, ok := field.Value.(string); !ok {
+				return errors.New("field " + key + " is not string")
+			}
+		case DocumentFieldTypeNumber:
+			if _, ok := field.Value.(int64); !ok {
+				if _, ok := field.Value.(float64); !ok {
+					return errors.New("field " + key + " is not number")
+				}
+			}
+		case DocumentFieldTypeBool:
+			if _, ok := field.Value.(bool); !ok {
+				return errors.New("field " + key + " is not bool")
+			}
+		case DocumentFieldTypeArray:
+			if _, ok := field.Value.([]any); !ok {
+				return errors.New("field " + key + " is not array")
+			}
+		}
+	}
+
+	return nil
+}
+
 // Put Updated signature of the function,
 // added return of an error if key filed is not exists in Document
-func Put(doc Document) (bool, error) {
+func Put(doc Document) error {
 	key, exists := doc.Fields["key"]
 
 	if !exists || key.Type != DocumentFieldTypeString {
-		return false, errors.New("can not put document without key field")
+		return errors.New("can not put document without key field")
+	}
+
+	if err := doc.validate(); err != nil {
+		return err
 	}
 
 	documentStore[key.Value.(string)] = doc
 
-	return true, nil
+	return nil
 }
 
 func Get(key string) (*Document, bool) {
