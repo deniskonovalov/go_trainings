44020d670f9770c436a7a359c9480a2e962f90e3
Denys.Konovalov <denis.konov93@gmail.com>
2025-04-22 13:36:28 +0300

    Add LRUCache solution

---
+++ lesson_08/cmd/app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"lesson_08/lru"
+)
+
+func main() {
+	c := lru.NewLruCache(3)
+
+	//fmt.Printf("%+v\n", c)
+
+	c.Put("key", "value")
+	//fmt.Printf("%+v\n", c)
+
+	c.Put("key2", "value2")
+	c.Put("key4", "value11111")
+	c.Put("key5", "value11111")
+	c.Put("key6", "value11111")
+	c.Put("key7", "value11111")
+
+	c.Put("key3", "value11111")
+
+	//
+	fmt.Println(c.Get("key7"))
+	fmt.Println(c.Get("key"))
+
+}
---
+++ lesson_08/lru/cache.go
@@ -0,0 +1,71 @@
+package lru
+
+import (
+	"container/list"
+)
+
+type LruCache interface {
+	Put(key, value string)
+	Get(key string) (string, bool)
+}
+
+type CacheStorage struct {
+	items     map[string]*list.Element
+	capacity  int
+	cacheList *list.List
+}
+
+func NewLruCache(capacity int) LruCache {
+	cacheStorage := CacheStorage{
+		items:     make(map[string]*list.Element),
+		capacity:  capacity,
+		cacheList: list.New(),
+	}
+
+	return &cacheStorage
+}
+
+type cacheItem struct {
+	key   string
+	value string
+}
+
+func (c *CacheStorage) Put(key, value string) {
+	if elem, exists := c.items[key]; exists {
+		c.cacheList.MoveToFront(elem)
+		elem.Value = cacheItem{
+			key:   key,
+			value: value,
+		}
+
+		return
+	}
+
+	if len(c.items) >= c.capacity {
+		last := c.cacheList.Back()
+		if last != nil {
+			item := last.Value.(cacheItem)
+			delete(c.items, item.key)
+			c.cacheList.Remove(last)
+		}
+	}
+
+	item := cacheItem{
+		key:   key,
+		value: value,
+	}
+
+	elem := c.cacheList.PushFront(item)
+	c.items[key] = elem
+}
+
+func (c *CacheStorage) Get(key string) (string, bool) {
+	elem, exists := c.items[key]
+	if !exists {
+		return "", false
+	}
+
+	c.cacheList.MoveToFront(elem)
+
+	return elem.Value.(cacheItem).value, true
+}
